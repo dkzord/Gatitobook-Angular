65d51e98a8b3e1c3d5ee686c500700a66045f783
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-04-07 20:08:48 -0300

    Feat: comment created comments

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { TokeService } from '../autenticacao/toke.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +17,27 @@ export class AnimaisService {
   constructor(private http: HttpClient, private tokeService: TokeService) { }
 
   listaDoUsuario(nomeUsuario: string): Observable<Animais> {
-    const token = this.tokeService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`, {
-      headers,
-    });
+    return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`);
+  }
+
+  buscaPorId(id: number): Observable<Animal>{
+    return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluiAnimal(id: number): Observable<Animal>{
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean>{
+    return this.http.post(`${API}/photos/${id}/like`,
+    {},
+    { observe: 'response' }
+    ).pipe(
+      mapTo(true),
+      catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+      })
+    )
   }
 
 }
---
+++ src/app/autenticacao/autenticacao.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokeService } from './toke.service';
+
+@Injectable()
+export class AutenticacaoInterceptor implements HttpInterceptor {
+
+  constructor(private tokenService: TokeService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    if(this.tokenService.possuiToken()){
+      const token = this.tokenService.retornaToken();
+      const headers = new HttpHeaders().append('x-access-token', token);
+      request = request.clone({ headers });
+    }
+
+    return next.handle(request);
+  }
+}
